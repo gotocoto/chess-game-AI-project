6ad163a094309fa57c4b47013b5fe3b407354fa3
House <Ben House>
2023-12-15 00:39:57 -0500

    Unsure if values are correct

--- solution_q1.py
+++ solution_q1.py
@@ -23,38 +23,44 @@ A_J = dict([
     ("-+",.05),
     ("--",.95)
 ])
-
+pm = ["+","-"]
+p = "+"
 JA_BE = {}
-for con in ["++","+-","-+","--"]:
-    for act in ["++","+-","-+","--"]:
-        JA_BE[act+con]=A_J[act]*A_BE[act[0]+con]
+for j in pm:
+    for a in pm:
+        for b in pm:
+            for e in pm:
+                JA_BE[j+a+b+e]=A_J[a+j]*A_BE[a+b+e]
 
 J_BE = {}
-for con in ["++","+-","-+","--"]:
-    for act in ["+","-"]:
-        J_BE[act+con]= JA_BE[act+"+"+con]+JA_BE[act+"-"+con]
+for j in pm:
+    for b in pm:
+        for e in pm:
+            J_BE[j+b+e]= JA_BE[j+"+"+b+e]+JA_BE[j+"-"+b+e]
 
 EJ_B = {}
-for con in ["+","-"]:
-    for act in ["++","+-","-+","--"]:
-        EJ_B[act+con]= E[act[0]]*J_BE[act[0]+con+act[0]]
+for e in pm:
+    for j in pm:
+        for b in pm:
+            EJ_B[e+j+b]= E[e]*J_BE[j+b+e]
 
 J_B = {}
-for con in ["+","-"]:
-    for act in ["+","-"]:
-        J_B[act+con]=EJ_B[act+"+"+con]+EJ_B[act+"-"+con]
+for j in pm:
+    for b in pm:
+        J_B[j+b]=EJ_B["+"+j+b]+EJ_B["-"+j+b]
 
 JB = {}
-for act in ["++","+-","-+","--"]:
-    JB[act]=B[act[1]]*J_B[act]
+for j in pm:
+    for b in pm:
+        JB[j+b]=B[b]*J_B[j+b]
 
 J = {}
-for act in ["+","-"]:
-    J[act]=JB[act+"+"]+JB[act+"-"]
+for j in pm:
+    J[j]=JB[j+"+"]+JB[j+"-"]
 
 B_J = {}
-for con in ["+","-"]:
-    for act in ["+","-"]:
-        B_J[act+con]=JB[con+act]/J[con]
+for b in pm:
+    for j in pm:
+        B_J[b+j]=JB[j+b]/J[j]
 
 print(print(json.dumps(B_J, indent = 4)))
